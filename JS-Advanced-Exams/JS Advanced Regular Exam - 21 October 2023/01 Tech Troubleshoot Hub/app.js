422ad2bc4d4718085a0466111ff319ecae64fd84
Tihomir Nedelchev <tihomir.nedelchev86@abv.bg>
2023-12-04 11:28:17 +0200

    JavaScript DOM

--- JS-Advanced-Exams/JS Advanced Regular Exam - 21 October 2023/01 Tech Troubleshoot Hub/app.js
+++ JS-Advanced-Exams/JS Advanced Regular Exam - 21 October 2023/01 Tech Troubleshoot Hub/app.js
@@ -1,9 +1,22 @@
-window.addEventListener('load', solution);
+window.addEventListener("load", solution);
 
 function solution() {
-  //TODO...
+    const allInputFields = {
+        employee: document.querySelector("#employee"),
+        category: document.querySelector("#category"),
+        urgency: document.querySelector("#urgency"),
+        team: document.querySelector("#team"),
+        description: document.querySelector("#description"),
+    };
+
+    const addBtn = document.querySelector("#add-btn");
+    addBtn.addEventListener("click", addTroubleshoot);
+
+    function addTroubleshoot(e) {
+        e.preventDefault();
+        if (Object.values(allInputFields).some((t) => t.value === "")) {
+            return;
+        }
+        console.log("yes");
+    }
 }
-
-
-    
-    
